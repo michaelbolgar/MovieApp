fd9cb438c519663681edaee40a8e074350f527b7
Kirilloao <kirilltaraturin747@gmail.com>
2023-12-25 13:44:10 +0100

    create generalCell

--- MovieApp/MovieApp/Moduls/Profile/View/Cell/GeneralCell.swift
+++ MovieApp/MovieApp/Moduls/Profile/View/Cell/GeneralCell.swift
@@ -7,17 +7,8 @@
 
 import UIKit
 
-class GeneralCell: UITableViewCell {
+final class GeneralCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
