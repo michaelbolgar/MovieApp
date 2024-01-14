988d0aeef0ca2abc28701f04032859aacfd28f5d
Nikita Kornuk <kornyk62@gmail.com>
2024-01-14 10:20:46 +0100

    add app icon and launch screen

--- MovieApp/MovieApp/Application/AppDelegate.swift
+++ MovieApp/MovieApp/Application/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 3.0)
 
         return true
     }
