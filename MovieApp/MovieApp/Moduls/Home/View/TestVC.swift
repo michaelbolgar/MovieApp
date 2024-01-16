e9bff1c54e2cc69b579c0eb17dcb3a70889939fd
Kirilloao <kirilltaraturin747@gmail.com>
2024-01-16 11:45:55 +0100

    add new tokens

---
+++ MovieApp/MovieApp/Moduls/Home/View/TestVC.swift
@@ -0,0 +1,29 @@
+//
+//  TestVC.swift
+//  MovieApp
+//
+//  Created by Kirill Taraturin on 15.01.2024.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MovieApp/MovieApp/Services/Network/API.swift
+++ MovieApp/MovieApp/Services/Network/API.swift
@@ -3,17 +3,24 @@ import Foundation
 struct API {
     static let scheme = "https"
     static let host = "api.kinopoisk.dev"
-    static let apiKey = Token.fifth
+    static let apiKey = Token.fourteen
 }
 
 struct Token {
-    static let first = "AGKSKKE-31E4TH5-GRPZKKD-K00YT2E"
-    static let second = "SECH8RE-B9ZM292-G4TH71N-7AEWMFN"
-    static let third = "486MEQ2-APQ41CP-GT0N0R2-MWE59JA"
-    static let fourth = "QC4AKM6-2YZ42H6-NQT8M9Z-P1MC2JP"
-    static let fifth = "DKE1X88-Q19MYCZ-KHTKXKH-F3SAAPM"
-    static let sixth = "TRF6JF1-JVH4RW9-K5Q58CC-MFB4GKM"
-    static let seventh = "1C9HKRM-E514EPT-JEV341T-S2H6MMA"
+    static let one = "AGKSKKE-31E4TH5-GRPZKKD-K00YT2E"
+    static let two = "SECH8RE-B9ZM292-G4TH71N-7AEWMFN"
+    static let three = "486MEQ2-APQ41CP-GT0N0R2-MWE59JA"
+    static let four = "QC4AKM6-2YZ42H6-NQT8M9Z-P1MC2JP"
+    static let five = "DKE1X88-Q19MYCZ-KHTKXKH-F3SAAPM"
+    static let six = "TRF6JF1-JVH4RW9-K5Q58CC-MFB4GKM"
+    static let eight = "1C9HKRM-E514EPT-JEV341T-S2H6MMA"
+    static let nine = "CA0Y6V3-4JPMCKW-PTSQ135-YPKXT4X"
+    static let ten = "1TMH0NP-45VMCVB-J4Y4RF6-TZQ7ZW7"
+    static let eleven = "YGNWPD2-KY7MSX4-KTC4RS6-H8W2C2M"
+    static let twelve = "BR9GX9R-2TD4AQX-H67HE11-CSGFVP4"
+    static let thirteen = "4M3ZC0R-XYV44S1-QE7HWZA-ASPTETW"
+    static let fourteen = "MEY4BQK-S3G4X7A-QWH2AEP-MKCHDYX"
+    
 }
 
 //you can generate more apiKeys on https://t.me/kinopoiskdev_bot if you've exceeded the daily requests limit (200 requests/day)
